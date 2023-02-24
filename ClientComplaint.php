7d557d190ecdf51ebf02720f0a91f9f727a9ee80
PALR-1216 <palr12162000@gmail.com>
2023-02-24 16:40:23 -0400

    fgf

---
+++ ClientComplaint.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Make Complaint</title>
+</head>
+<body>
+
+<h1>Make a Complaint</h1>
+    
+</body>
+</html>
--- _ClientNavbar.php
+++ _ClientNavbar.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome User</title>
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
 
 </head>
@@ -32,6 +31,11 @@
           
           <li class="nav-item">
             <a class="nav-link" href="allTenants.php" tabindex="-1" aria-disabled="true">My Balance</a>
+          </li>
+
+
+          <li class="nav-item">
+            <a class="nav-link" href="ClientPay.php" tabindex="-1" aria-disabled="true">Pay Now</a>
           </li>
 
           <li class="nav-item">
